cbb98e8f6b1f92bf9dbcee8e4644fe16558b3051
CynthiaVO <156707934+CynthiaVO@users.noreply.github.com>
2024-02-22 14:40:09 -0800

    script passes all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(textString, repeats) {
+    let resultString = '';
+    if (repeats >= 0){
+        for (let i = 0; i < repeats; i++){
+            resultString += textString;
+        }
+        return resultString;
+    }
+    else {
+        return 'ERROR';
+    }
+        
 };
+repeatString("hey",3);
 
 // Do not edit below this line
 module.exports = repeatString;
