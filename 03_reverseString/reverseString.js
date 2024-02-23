b4a0c468c4ff700b495352e1740df86fc649bc82
CynthiaVO <156707934+CynthiaVO@users.noreply.github.com>
2024-02-23 09:41:01 -0800

    reverses a string and passes all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
-
+function reverseString(inputS) {
+    const inputString = inputS;
+    let reverseS = "";
+    //put the string into an array
+    stringArray = inputString.split("");
+    stringArray.reverse();
+    for (let x in stringArray){
+        reverseS += stringArray[x];
+    }
+    return reverseS;
+  
 };
+reverseString("h3llo There 007!")
 
 // Do not edit below this line
 module.exports = reverseString;
