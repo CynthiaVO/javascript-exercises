b386a12a31ee37cf618ce11bff2651a5a349ae2c
CynthiaVO <156707934+CynthiaVO@users.noreply.github.com>
2024-02-25 08:03:44 -0800

    all tests pass

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,14 +2,14 @@ const removeFromArray = function(arrayToTest, ...args) {
     args.forEach(arg => {
     
         if (arrayToTest.includes(arg)){
-           console.log(arg + " is in the array");
+           //console.log(arg + " is in the array");
            
            myIndex = arrayToTest.indexOf(arg);
-           console.log("the index of " + arg + " is " + myIndex);
-           console.log("arrayToTest before splicing: "+ arrayToTest);
+           //console.log("the index of " + arg + " is " + myIndex);
+           //console.log("arrayToTest before splicing: "+ arrayToTest);
            
            arrayToTest = arrayToTest.toSpliced(myIndex,1 );
-           console.log("after splicing: " + arrayToTest);
+           //console.log("after splicing: " + arrayToTest);
        } 
    })
    return arrayToTest;
