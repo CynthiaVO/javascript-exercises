1d6450652ab86b332fbb6496279fc57aaeb208f9
CynthiaVO <156707934+CynthiaVO@users.noreply.github.com>
2024-02-27 10:41:09 -0800

    all tests pass

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,22 @@
-const leapYears = function() {
-
+const leapYears = function(testYear) {
+    
+    //console.log(testYear % 100 == 0);
+    //console.log(!(testYear % 100 == 0));
+    //console.log(testYear % 400 == 0);
+    //console.log(testYear % 4 == 0);
+  
+    if (testYear % 4 == 0 && !(testYear % 100 == 0)){
+        return true;
+    }
+    else if (testYear % 100 == 0 && testYear % 400 == 0){
+        return true;
+    }
+    else {
+        return false;
+    }
 };
-
+//const yearTested = 700;
+//document.getElementById("demo").innerHTML =
+//"is " + yearTested + " a leap year? " + leapYears(yearTested);
 // Do not edit below this line
 module.exports = leapYears;
