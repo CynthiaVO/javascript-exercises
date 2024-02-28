2a89a7f64fb72dcdc6ecf6d525bd6a3452a0fc2d
CynthiaVO <156707934+CynthiaVO@users.noreply.github.com>
2024-02-28 07:57:51 -0800

    all tests pass

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(x) {
+  x  = (x - 32) * 5/9;
+  if (x==0){
+    
+    return parseFloat(x.toFixed(0));
+  }
+  else {
+    return parseFloat(x.toFixed(1));
+  }
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(x) {
+  x = (x * 9/5 + 32);
+  return parseFloat(x.toFixed(1));
 };
-
+//document.getElementById("demo").innerHTML = convertToCelsius(100);
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
